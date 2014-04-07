d875e60e748864bfd5484f1867e672afc670b3e7
thilp <thilp@thilp.net>
2014-04-08 01:28:02 +0200

    (minor) add some doc about configuration

--- lib/Dancer2/Template/TextTemplate.pm
+++ lib/Dancer2/Template/TextTemplate.pm
@@ -35,19 +35,37 @@ is created I<for each> template. Therefore, if:
 * you don't use each template just once;
 
 then it may be interesting to B<cache> Text::Template instances for later use.
+Since these conditions seem to be common, this engine uses a cache (I<via>
+L<CHI>) B<by default>.
 
-If so, you should specify a B<timeout> after which cached Text::Template
-instances are to be refreshed, since you might have changed your template
-sources without restarting Dancer2.
+If you're OK with caching, you should specify a B<timeout> (C<expires>) after
+which cached Text::Template instances are to be refreshed, since you might
+have changed your template sources without restarting Dancer2. Use the value
+C<0> to tell the engine that templates never expire.
 
 To enable caching in your C<config.yml>:
 
     template: text_template
     engines:
         text_template:
-            caching: 1      # default
-            expires: 3600   # in seconds; default
-                            # (use 0 for no expiration)
+            caching: 1                  # default
+            expires: 3600               # in seconds; default; 0 to disable
+            cache_stringrefs: 1         # default
+            delimiters: [ "{", "}" ]    # default
+
+Just like with L<Dancer2::Template::Toolkit>, you can pass templates either as
+filenames (for a template file) or string references ("string-refs", which are
+dereferenced and used as the template's content). In some cases, you may want
+to disable caching just for string-refs: for instance, if you generate a lot
+of templates on-the-fly and use them only once, caching them is useless and
+fills your cache. You can therefore disable caching for string-refs only by
+setting C<cache_stringrefs> to C<0>.
+
+The C<delimiters> option allows you to specify a custom delimiters pair
+(opening and closing) for your templates. See the L<Text::Template>
+documentation for more about delimiters, since this module just pass them to
+Text::Template. This option defaults to C<{> and C<}>, meaning that in C<< a
+{b} c >>, C<b> (and only C<b>) will be interpolated.
 
 =cut
 
