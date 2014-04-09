1e18dfb43980edbfa5110d9dd645062af16d0b0b
thilp <thilp@thilp.net>
2014-04-09 04:01:32 +0200

    (minor) warns about alpha-state in TextTemplate.pm's doc

--- lib/Dancer2/Template/TextTemplate.pm
+++ lib/Dancer2/Template/TextTemplate.pm
@@ -24,6 +24,9 @@ To use this engine, you may configure L<Dancer2> via C<config.yml>:
 
 =head1 DESCRIPTION
 
+B<This is an alpha version: it basically works, but it has not been
+extensively tested and it misses interesting features.>
+
 This template engine allows you to use L<Text::Template> in L<Dancer2>.
 
 Contrary to other template engines (like L<Template::Toolkit>), where I<one>
